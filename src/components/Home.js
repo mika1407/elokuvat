8d00111b1c2914b7470b254487e6ff9a186fac84
mika tiihonen <koneautomaatio@hotmail.com>
2021-09-07 16:22:05 +0300

    searchBar tehty

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,29 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
+
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
 
   console.log(state);
 
   return(
     <>
-    {state.results[0] ? (
+    {!searchTerm && state.results[0] ? (
     <HeroImage 
       image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
       title={state.results[0].original_title}
       text={state.results[0].overview}
       />
     ) : null}
-    <Grid header='Suositut Elokuvat:'>
+    <SearchBar setSearchTerm={setSearchTerm}/>
+    <Grid header={searchTerm ? 'Haun Tulos' : 'Suositut Elokuvat'}>
       {state.results.map(movie => (
           <Thumb
               key={movie.id}
